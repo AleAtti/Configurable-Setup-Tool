6094f41240d483de51340a3aaab6aa6c5cb9404d
alexander <alexanderattisani@gmail.com>
2025-05-10 22:39:06 +0200

    feat: add JsonModel.cs

---
+++ SetupTool/model/JsonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ServiceInstaller.model
+{
+    public class JsonModel<T>
+    {
+        public static T FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public static string ToJson(T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}
