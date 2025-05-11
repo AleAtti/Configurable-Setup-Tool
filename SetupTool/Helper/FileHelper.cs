b570ca2928d4c88a4dfba1834d97eb26a280b7c1
alexander <alexanderattisani@gmail.com>
2025-05-11 21:59:40 +0200

    feat: add FileHelper.cs, minor changes to JsonModel and json file

---
+++ SetupTool/Helper/FileHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInstaller.Helper
+{
+    public class FileHelper
+    {
+        public static string GetFileName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException(nameof(path), "Path cannot be null or empty");
+            }
+            return Path.GetFileName(path);
+        }
+        public static string GetDirectoryName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException(nameof(path), "Path cannot be null or empty");
+            }
+            return Path.GetDirectoryName(path);
+        }
+
+        public static string GetRelativPath(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName), "File name cannot be null or empty");
+            }
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string relativePath = Path.Combine(currentDirectory, fileName);
+            return relativePath;
+        }
+    }
+}
--- SetupTool/model/JsonModel.cs
+++ SetupTool/model/JsonModel.cs
@@ -9,13 +9,26 @@ namespace ServiceInstaller.model
 {
     public class JsonModel<T>
     {
+
+        private static JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
         public static T FromJson(string json)
         {
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new ArgumentNullException(nameof(json), "JSON string cannot be null or empty");
+            }
             return JsonSerializer.Deserialize<T>(json);
         }
 
         public static string ToJson(T obj)
         {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Object cannot be null");
+            }
             return JsonSerializer.Serialize(obj);
         }
     }
