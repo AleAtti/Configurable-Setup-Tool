2dfaa2e41b1d9c2e21d3b1ae4d3f2fea080d2858
alexander <alexanderattisani@gmail.com>
2025-05-12 21:22:48 +0200

    feat: add read and write frm current AppDir

--- SetupTool/Helper/FileHelper.cs
+++ SetupTool/Helper/FileHelper.cs
@@ -26,15 +26,46 @@ namespace ServiceInstaller.Helper
             return Path.GetDirectoryName(path);
         }
 
-        public static string GetRelativPath(string fileName)
+        public static string GetAppCurrentDir(string fileName)
         {
             if (string.IsNullOrEmpty(fileName))
             {
                 throw new ArgumentNullException(nameof(fileName), "File name cannot be null or empty");
             }
+
             string currentDirectory = Directory.GetCurrentDirectory();
             string relativePath = Path.Combine(currentDirectory, fileName);
             return relativePath;
+        }
+
+        public static string ReadFileFromCurrentDir(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName), "File name cannot be null or empty");
+            }
+
+            
+            string relativePath = GetAppCurrentDir(fileName);
+
+            if (!File.Exists(relativePath))
+            {
+                throw new FileNotFoundException(relativePath);
+            }
+            return File.ReadAllText(fileName, Encoding.UTF8);
+        }
+
+        public static void WriteFileFromCurrentDir(string fileName, string content)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException(fileName);
+            }
+
+            using (StreamWriter writer = new StreamWriter(GetAppCurrentDir(fileName), append: false, Encoding.UTF8)) { 
+                writer.Write(content);
+            }
+            
         }
     }
 }
